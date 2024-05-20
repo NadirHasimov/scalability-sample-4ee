ba7909cd86af0f27006c99a2f3d1a9ac848cbf78
NadirHasimov <nadir.hashimov@outlook.com>
2024-05-20 19:13:59 +0400

    Add project files.

---
+++ scalability-sample-4ee/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace scalability_sample_4ee.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
